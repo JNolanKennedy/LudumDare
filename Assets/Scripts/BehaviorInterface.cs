8ded6a8353fed57f5d256c5ef194d1ba6daf4fdf
Alexander Freedman <alexander.freedman@gmail.com>
2015-08-21 20:37:05 -0700

    Adds Interface and script folder.

---
+++ Assets/Scripts/BehaviorInterface.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface BehaviorInterface
+{
+	void Shoot();
+	void Move(int Direction);
+	bool TakeDamage();
+	void OnDeath();
+	void Infect();
+	void Upgrade();
+}
