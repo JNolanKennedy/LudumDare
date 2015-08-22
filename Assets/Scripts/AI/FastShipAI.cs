8e9ebcddf2521945eb4b378e8877aa3c5d0dc3f7
LNunamaker <nunamal@students.wwu.edu>
2015-08-22 12:15:42 -0700

    reorganized scripts, fixed scripts

---
+++ Assets/Scripts/AI/FastShipAI.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FastShipAI : MonoBehaviour, AI {
+    ShipInterface currentShip;
+    bool isPlayer = false;
+    Transform player;
+    float cooldown = .3f;
+
+	// Use this for initialization
+	void Start () {
+        currentShip = this.GetComponent(typeof(ShipInterface)) as ShipInterface;
+        if (this.transform.parent != null)
+        {
+            isPlayer = true;
+        }
+        else
+        {
+            this.transform.Rotate(Vector3.forward * 180);
+            player = GameObject.Find("Player").transform.GetChild(0);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isPlayer == false)
+        {
+            AdjustPosition();
+            if(cooldown <= 0)
+            {
+                currentShip.Shoot();
+                cooldown = .3f;
+            }
+            cooldown = cooldown - Time.deltaTime;
+            
+        }   
+	}
+
+    private void AdjustPosition()
+    {
+        currentShip.Move(new Vector2(-1f, 0));
+        Vector3 dir = player.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public void Infect()
+    {
+        //do nothing
+    }
+}
