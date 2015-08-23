08324c1248d3dac2b608e5f5b3b42724c9677a5a
Alexander Freedman <alexander.freedman@gmail.com>
2015-08-23 12:44:09 -0700

    LASOOOOOORRR SHIP!

--- Assets/Scripts/Ships/LaserShip.cs
+++ Assets/Scripts/Ships/LaserShip.cs
@@ -3,24 +3,36 @@ using System.Collections;
 
 public class LaserShip : BaseShip
 {
-	private int Charge;
+	private float Charge;
+	private Vector2 laserpoint;
+	private Vector2 endpoint;
+	LineRenderer LR;
+	private bool Shooting;
 
 	// Use this for initialization
 	public override void overrideStart() {
 		this.HP = 3;
 		this.Shield = 1;
-		this.Speed = .01f;
+		this.Speed = .1f;
 		this.AttachPoint = new Vector2(0, 3);
 		this.Charge = 0;
 		this.source = this.GetComponent<AudioSource>();
+		this.laserpoint = new Vector2 (1f, -.01f);
+		this.endpoint = new Vector2 (0f, -10f);
+		this.Shooting = false;
+		LR = this.gameObject.AddComponent<LineRenderer> ();
+		LR.SetWidth (.8f, .8f);
+	}
+
+	private void createLaser ()
+	{
+		LR.enabled = true;
+		LR.SetPosition (0, this.transform.position+(Vector3)this.laserpoint);
 	}
 
 	public override void Shoot()
 	{
-		this.Charge += (int)Time.deltaTime;
-		if (this.Charge >= 2) {
-			this.Charge = 0;
-		}
+		this.Shooting = true;
 	}
 
 	public override void OnTriggerEnter2D(Collider2D coll)
@@ -28,6 +40,40 @@ public class LaserShip : BaseShip
 		if (coll.tag == "Enemy" && this.tag == "PlayerShip") {
 			ShipInterface si = coll.gameObject.GetComponent(typeof(ShipInterface)) as ShipInterface;
 			si.TakeDamage(1);
+		}
+	}
+
+	public void Update()
+	{
+		if (this.Shooting) {
+			this.Speed = 0f;
+			this.Charge += Time.deltaTime;
+			if (this.Charge >= 1) {
+				this.createLaser();
+				this.Speed = 0;
+				RaycastHit2D[] hit = Physics2D.RaycastAll(this.transform.position, this.transform.right, 200f);
+				int size = hit.Length;
+				for(int i = 0; i < size; i++) {
+					if(hit[i].transform.name == "boundwall4" || hit[i].transform.name == "boundwall2")
+					{
+						endpoint = (Vector2)hit[i].point;
+						Debug.Log("HERE: " + endpoint);
+					}
+				}
+				if(this.endpoint.x == 0f && this.endpoint.y == -10f)
+				{
+					this.endpoint = (Vector2)this.transform.position + new Vector2(100,0);
+				}
+				Debug.Log("THERER: " + endpoint);
+				LR.SetPosition(1, (Vector3)endpoint);
+			}
+			if (this.Charge >= 2) {
+				LR.enabled = false;
+				this.Charge = 0;
+				this.Speed = .1f;
+				this.Shooting = false;
+				this.endpoint = new Vector2 (0f, -10f);
+			}
 		}
 	}
 }
