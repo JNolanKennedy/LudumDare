43c53c698338ecd53185100964a6d2cc2032c6a9
LNunamaker <nunamal@students.wwu.edu>
2015-08-22 18:54:40 -0700

    Basic UI

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+    Text HP;
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponentsInChildren<Text>()[0].text = "HP : ";
+        gameObject.GetComponentsInChildren<Text>()[1].text = "Shields : ";
+        player = GameObject.FindWithTag("PlayerShip");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        string hp = (player.GetComponent(typeof(BaseShip)) as BaseShip).getHP().ToString();
+        string shields = (player.GetComponent(typeof(BaseShip)) as BaseShip).getShields().ToString();
+        gameObject.GetComponentsInChildren<Text>()[0].text = "HP : " + hp;
+        gameObject.GetComponentsInChildren<Text>()[1].text = "Shields : " + shields;
+	}
+}
