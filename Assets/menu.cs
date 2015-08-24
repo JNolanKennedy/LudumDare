8ef54e9660133e78f9d07c53272f0276acad0edb
Jacob Kennedy <JNolanKennedy@Gmail.com>
2015-08-24 13:20:56 -0700

    final push before testing?

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -14,7 +14,7 @@ public class menu : MonoBehaviour {
 
     public void nextText()
     {
-        instructions.GetComponentInChildren<Text>().text = "Enemy Shields are down when the bar above them is entirely red \n\n Don't run into enemies while their shields are up or you'll die \n\n If your ship is blown up you still have a chance to release your host and survive \n\n Good Luck bringing death to humanity";
+        instructions.GetComponentInChildren<Text>().text = "Enemy Shields are down when the bar above them is entirely red \n\n Don't run into enemies while their shields are up or you'll die \n\n If your ship is blown up you still have a chance to release your host and survive \n\n Good Luck and survive!";
         next.SetActive(false);
     }
 
@@ -22,7 +22,11 @@ public class menu : MonoBehaviour {
     {
         
         instructions.SetActive(true);
-        instructions.GetComponentInChildren<Text>().text = "dafdsa\n Don't run into enemies while their shields are up or you'll die \n\n If your ship is blown up you still have a chance to release your host and survive \n\n Good Luck bringing death to humanity";
+		instructions.GetComponentInChildren<Text> ().text = "You are a highly-advanced lifeform created for the purpose of destroying the human race.\n\n" +
+			"Move with WASD or the arrow keys.\n\n" +
+			"Use Spacebar to shoot while inside of a ship and left shift to release your host.\n\n" +
+			"ESC to pause the game.\n\n" +
+			"You can only infest ships when their shield is depleted.";
         next.SetActive(true);
     }
 
