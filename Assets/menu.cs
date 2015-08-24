6c324097aeb0d6ccefc0acaba2a779e9db94b9d8
LNunamaker <nunamal@students.wwu.edu>
2015-08-24 13:14:36 -0700

    improved menu

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -7,11 +7,6 @@ public class menu : MonoBehaviour {
     public GameObject instructions;
     public GameObject next;
 
-    void Start()
-    {
-        instructions.SetActive(false);
-    }
-
     public void nextLevel()
     {
         Application.LoadLevel(1);
@@ -19,13 +14,15 @@ public class menu : MonoBehaviour {
 
     public void nextText()
     {
-        gameObject.GetComponent<Text>().text = "Enemy Shields are down when the bar above them is entirely red \n\n Don't run into enemies while their shields are up or you'll die \n\n If your ship is blown up you still have a chance to release your host and survive \n\n Good Luck bringing death to humanity";
+        instructions.GetComponentInChildren<Text>().text = "Enemy Shields are down when the bar above them is entirely red \n\n Don't run into enemies while their shields are up or you'll die \n\n If your ship is blown up you still have a chance to release your host and survive \n\n Good Luck bringing death to humanity";
         next.SetActive(false);
     }
 
     public void spawnInstructions()
     {
+        
         instructions.SetActive(true);
+        instructions.GetComponentInChildren<Text>().text = "dafdsa\n Don't run into enemies while their shields are up or you'll die \n\n If your ship is blown up you still have a chance to release your host and survive \n\n Good Luck bringing death to humanity";
         next.SetActive(true);
     }
 
